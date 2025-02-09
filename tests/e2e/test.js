db1b569ff85836f28191ddf4acb92130013a4d3b
Mitch MacKenzie <mitchmac@gmail.com>
2025-02-08 21:12:33 -0400

    Specific host tests

--- tests/e2e/test.js
+++ tests/e2e/test.js
@@ -5,6 +5,15 @@ exports.handler = async function (event, context, callback) {
     const docRoot = path.join(process.cwd(), 'wp');
     const routerScript = path.join(process.cwd(), 'router.php');
   
+    if (process.env['HOST']) {
+        if (process.env['HOST'] === 'Vercel') {
+            process.env['VERCEL'] = 1;
+        }
+
+        if (process.env['HOST'] === 'Netlify') {
+            process.env['SITE_NAME'] = 1;
+        }
+    }
     const response = await serverlesswp({event: event, docRoot: docRoot, routerScript: routerScript});
    
     return response;
