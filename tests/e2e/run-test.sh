74ac743bf59ee5e177581206a443f29d6a55c438
Mitch MacKenzie <mitchmac@gmail.com>
2025-02-08 21:14:53 -0400

    Fix container name

--- tests/e2e/run-test.sh
+++ tests/e2e/run-test.sh
@@ -2,4 +2,4 @@
 
 HOST=${HOST:-Vercel}
 
-docker run -e HOST=$HOST -p 9000:8080 -d --name serverlesswp docker-lambda-serverlesswp
+docker run -e HOST=$HOST -p 9000:8080 -d --name serverlesswp-test docker-lambda-serverlesswp
